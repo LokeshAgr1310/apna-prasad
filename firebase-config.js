e8396dab253fdd732620d8a07920874486c5dc10
LokehAgr1310 <lokeshagr131003@gmail.com>
2022-08-01 18:52:36 +0530

    Loaded from env

--- firebase-config.js
+++ firebase-config.js
@@ -3,13 +3,13 @@ import { getFirestore } from 'firebase/firestore'
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const firebaseConfig = {
-    apiKey: "AIzaSyCa4ZEci887ETOODcVbBaqA5vyeIkiVgvw",
-    authDomain: "apna-prasad.firebaseapp.com",
-    projectId: "apna-prasad",
-    storageBucket: "apna-prasad.appspot.com",
-    messagingSenderId: "259851341407",
-    appId: "1:259851341407:web:e907a296a7585f01c515ea",
-    measurementId: "G-D1BR4RS3YW"
+    apiKey: process.env.NEXT_PUBLIC_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig)
